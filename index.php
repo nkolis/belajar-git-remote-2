6645ef7c5529fb511a896f26f49f343b36a884fd
nkolis <nurkholis010@gmail.com>
2021-10-13 18:51:47 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae voluptate, eaque accusantium provident, delectus fuga in nostrum, assumenda deleniti impedit pariatur quo? Minima, ad placeat deleniti inventore rerum atque delectus alias dolores commodi omnis nemo possimus corporis repellendus in repudiandae. Dignissimos nisi architecto minima ipsa non quidem numquam quibusdam est id, obcaecati, explicabo exercitationem rerum commodi maxime esse dolorem, in natus sequi cupiditate? Ab similique, nemo placeat sed aut quaerat neque pariatur harum dolores id aliquid doloribus? Voluptatum perferendis facere perspiciatis autem excepturi repellat! Similique minus expedita, aut quae optio iure iste modi, quasi unde delectus labore tempore accusantium provident!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
